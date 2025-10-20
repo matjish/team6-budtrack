29623144048994ecc9849e504599897893734bd2
jaxxnine <jaxxnine.artist@gmail.com>
2025-10-20 14:50:34 +0200

    input styling

--- view.js
+++ view.js
@@ -226,19 +226,24 @@ function updateViewTransactions() {
     
         ${viewNavBar()}
         <p></p>
-        <div id="transactionBars">
+        <div class="transactionBars" id="transactionBars">
     `
     
     html += /*HTML*/`
-        <input value="${model.viewState.registration.year}" onchange="model.viewState.registration.year = this.value"/>
-        <input value="${model.viewState.registration.month}" onchange="model.viewState.registration.month = this.value"/>
-        <input value="${model.viewState.registration.category}" onchange="model.viewState.registration.category = this.value"/>
+    
+  <table class="transactionBar">
+  <tr>
+    <td><input value="${model.viewState.registration.year}" onchange="model.viewState.registration.year = this.value" /></td>
+    <td><input value="${model.viewState.registration.month}" onchange="model.viewState.registration.month = this.value" /></td>
+    <td><input value="${model.viewState.registration.category}" onchange="model.viewState.registration.category = this.value" /></td>
+    <td><input value="${model.viewState.registration.details.name}" onchange="model.viewState.registration.details.name = this.value" /></td>
+    <td><input value="${model.viewState.registration.details.amount}" onchange="model.viewState.registration.details.amount = this.value" /></td>
+    <td><input value="${model.viewState.registration.details.status}" onchange="model.viewState.registration.details.status = this.value" /></td>
+    <td><button onclick="addTransaction()" class="add-btn">+</button></td>
+  </tr>
+</table>
 
-        <input value="${model.viewState.registration.details.name}" onchange="model.viewState.registration.details.name = this.value"/>
-        <input value="${model.viewState.registration.details.amount}" onchange="model.viewState.registration.details.amount = this.value"/>
-        <input value="${model.viewState.registration.details.status}" onchange="model.viewState.registration.details.status = this.value"/>
-
-        <button onclick="addTransaction()">+</button>
+       
     `
     
     /*
